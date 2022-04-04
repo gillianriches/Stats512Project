d4eadba2b806de46b2549794e0751b57a3493fc4
Gillian Riches <martingillian4@gmail.com>
2022-04-04 15:38:56 -0600

    Imported crashprep and datamaker scripts that create the joined "crash" dataframe. Added aadt to the "crash" dataframe and a check_aadt function

---
+++ crashprep.R
@@ -0,0 +1,75 @@
+# The following is the functions that prepare the crash data to be joined
+
+# Keeps the following columns from the location file
+check_location <- function(df){
+  df %>%
+    select(crash_id = crash_id, 
+           crash_datetime = crash_datetime, 
+           crash_severity_id = crash_severity_id, 
+           light_condition_id = light_condition_id, 
+           weather_condition_id = weather_condition_id, 
+           manner_collision_id = manner_collision_id,
+           roadway_surf_condition_id = roadway_surf_condition_id,
+           roadway_junct_feature_id = roadway_junct_feature_id, 
+           roadway_contrib_circum_id = roadway_contrib_circum_id,
+           route = route)
+}
+
+# Keeps the following columns from the vehicle file
+check_vehicle <- function(df){
+  df %>%
+    select(crash_id = crash_id, 
+           vehicle_num = vehicle_num, 
+           vehicle_year = vehicle_year,
+           travel_direction_id = travel_direction_id, 
+           event_1_id = event_sequence_1_id, 
+           event_2_id = event_sequence_2_id, 
+           event_3_id = event_sequence_3_id, 
+           event_4_id = event_sequence_4_id,
+           most_harmful_event_id = most_harmful_event_id, 
+           vehicle_maneuver_id = vehicle_maneuver_id)
+}
+
+# Keeps the following columns from the rollups file
+check_rollups <- function(df){
+  df %>%
+    select(crash_id = crash_id, 
+           number_fatalities = number_fatalities, 
+           number_four_injuries = number_four_injuries,
+           number_three_injuries = number_three_injuries, 
+           number_two_injuries = number_two_injuries,
+           number_one_injuries = number_one_injuries,
+           pedalcycle_involved = pedalcycle_involved, 
+           pedestrian_involved = pedestrian_involved,
+           motorcycle_involved = motorcycle_involved,	
+           distracted_driving = distracted_driving,
+           speed_related = speed_related,
+           adverse_weather = adverse_weather,	
+           adverse_roadway_surf_condition = adverse_roadway_surf_condition,	
+           roadway_geometry_related = roadway_geometry_related,
+           roadway_departure = roadway_departure,	
+           overturn_rollover = overturn_rollover,	
+           route_type = route_type,	
+           night_dark_condition = night_dark_condition,	
+           single_vehicle = single_vehicle)
+}
+
+check_aadt <- function(df){
+  df %>%
+    mutate(route = RT_NUM) %>%
+    select(START_ACCU = START_ACCU,
+           END_ACCUM = END_ACCUM,
+           route,
+           AADT2020 = AADT2020,
+           AADT2019 = AADT2019,
+           AADT2018 = AADT2018,
+           AADT2017 = AADT2017,
+           AADT2016 = AADT2016,
+           AADT2015 = AADT2015,
+           AADT2014 = AADT2014,
+           DESC_ = DESC_)
+}
+
+
+
+
---
+++ datamaker.R
@@ -0,0 +1,63 @@
+
+#build the crash dataset
+crashprep <- function() {
+  location <- read_csv("data/Crash_Data_14_20.csv")
+  vehicle <- read_csv("data/Vehicle_Data_14_20.csv")
+  rollups <- read_csv("data/Rollups_14_20.csv")
+  aadt <- read_csv("data/AADT_Unrounded.csv")
+  
+  location <- check_location(location)
+  vehicle <- check_vehicle(vehicle)
+  rollups <- check_rollups(rollups)
+  aadt <- check_aadt(aadt)
+
+  
+  crash <- left_join(location,rollups,by='crash_id')
+  crash <- left_join(crash,vehicle,by='crash_id')
+  crash <- left_join(crash, aadt, by = 'route')
+  rm("location","vehicle","rollups", "aadt")
+  
+  crash
+}
+
+#build severity dataset
+buildSeverity <- function(crashData) {
+  crashData %>% 
+    select(3,6) %>%
+    mutate(collisionType = case_when(
+      manner_collision_id == 1 ~ "1 Angle",
+      manner_collision_id == 2 ~ "2 FrontToRear",
+      manner_collision_id == 3 ~ "3 HeadOn",
+      manner_collision_id == 4 ~ "4 SideSwipeSame",
+      manner_collision_id == 5 ~ "5 SideSwipeOpp",
+      manner_collision_id == 6 ~ "6 ParkedVeh",
+      manner_collision_id == 7 ~ "7 RearToSide",
+      manner_collision_id == 8 ~ "8 RearToRear",
+      manner_collision_id == 96 ~ "96 SingleVeh",
+      manner_collision_id == 97 ~ "97 Other",
+      manner_collision_id %in% c(99,89) ~ "99 Unknown"
+    ))
+}
+
+buildSeveritySummary <- function(severityData) {
+  severityData %>%
+    group_by(collisionType) %>% 
+    summarize(
+      mean = mean(crash_severity_id),
+      sd = sd(crash_severity_id)
+    )
+}
+
+
+
+fun_mean <- function(x){return(round(data.frame(y=mean(x),label=mean(x,na.rm=T)),digit=2))}
+
+makeBoxPlot <- function(severity){
+  ggplot(severity) +
+    aes(x = as.factor(manner_collision_id), y = as.numeric(crash_severity_id)) +
+    geom_boxplot(aes(fill = collisionType)) + 
+    stat_summary(fun.y = mean, geom="crossbar",colour="blue", size=.5) +
+    stat_summary(fun.data = fun_mean, geom="text", vjust=-0.7) + 
+    labs(x = "Collision ID", y = "Crash Severity") +
+    theme_bw()
+}
